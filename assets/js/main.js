e073f2bd3ab707bb8ed0dae417062b68247aaf2c
rathanont <rathanont.s@gmail.com>
2019-09-20 06:54:06 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,28 @@
 $(function () {
     // Header
-    $("header").sticky({topSpacing:0});
+    $("header").sticky({
+        topSpacing: 0,
+        zIndex: 1030,
+    });
 
     // Banner
     $('#banner .slider').slick({
         dots: true,
         arrows: false,
+    });
+
+    // Best seller
+    $('#bestSeller .slider').slick({
+        slidesToShow: 3,
+        dots: true,
+        prevArrow: `<a href='#' class='slick-arrow-prev'><img src='./assets/images/Back_button.png' /></a>`,
+        nextArrow: `<a href='#' class='slick-arrow-next'><img src='./assets/images/Next_button.png' /></a>`,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                arrows: false,
+            }
+        }]
     });
 });
